aa43fd037ce72444deb254d4e3feeef249470b0d
Bekaa <nurallinbekjan@gmail.com>
2024-01-21 20:26:02 +0600

    Assignment 1 Nurallin Bekzhan SE-2307

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class helloworld {
+    public static void main(String []args) {
+        Scanner s= new Scanner(System.in);
+        float x= s.nextFloat();
+        System.out.println("ty vvel "+x);
+
+
+    }
+}
